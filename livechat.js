402cc613964cb4cc4755f0637c6a60452c5071c0
2006RKO <geremiah982@gmail.com>
2026-07-02 21:55:50 +0300

    Update livechat.js

--- livechat.js
+++ livechat.js
@@ -1,1 +1,11 @@
+const picker = document.querySelector("emoji-picker");
 
+const input = document.getElementById("messageInput");
+
+picker.addEventListener("emoji-click",(event)=>{
+
+input.value += event.detail.unicode;
+
+input.focus();
+
+});
