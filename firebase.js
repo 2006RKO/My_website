fd67f6e25b1104ae32a6a2ddb4df99ed675e3349
2006RKO <geremiah982@gmail.com>
2026-09-06 00:44:33 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,30 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD0k-VGDQCb07z8VU1lfj4PS0nQ1SJjt-U",
-  authDomain: "rko-website-design-8e1b6.firebaseapp.com",
-  projectId: "rko-website-design-8e1b6",
-  storageBucket: "rko-website-design-8e1b6.firebasestorage.app",
-  messagingSenderId: "18344688958",
-  appId: "1:18344688958:web:605719f065d65fddf1c06f",
-  measurementId: "G-JYRSY3P5DC"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
-const db = getFirestore(app);
-
-export { auth, db };
 // ==========================================
 // CHAPCY FIREBASE CONFIG
 // ==========================================
@@ -45,7 +18,7 @@ import { getDatabase } from
 
 const firebaseConfig = {
 
-    apiKey: "WEKA_API_KEY_YAKO_HAPA",
+    apiKey: "AIzaSyDIID2LpzjLiqaLeLJKgp-Vd7tNIyN-M1k",
 
     authDomain:
         "rko-website-design-2f792.firebaseapp.com",
@@ -60,10 +33,13 @@ const firebaseConfig = {
         "rko-website-design-2f792.firebasestorage.app",
 
     messagingSenderId:
-        "WEKA_MESSAGING_SENDER_ID",
+        "782567629866",
 
     appId:
-        "WEKA_APP_ID_YAKO_HAPA"
+        "1:782567629866:web:d6d80d454d0653ea8b4f53",
+
+    measurementId:
+        "G-KQ1EKYE7E7"
 
 };
 
