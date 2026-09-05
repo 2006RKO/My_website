04895656de18449985969441e7d7a132f046557e
2006RKO <geremiah982@gmail.com>
2026-09-05 23:50:47 +0300

    Update Efootball.js

--- Efootball.js
+++ Efootball.js
@@ -249,53 +249,6 @@ function setupPresence(){
 }
 
 
-// ==========================================
-// LOAD REALTIME MESSAGES
-// ==========================================
-
-function startMessages(){
-
-    if(messagesListenerStarted){
-
-        return;
-
-    }
-
-
-    messagesListenerStarted = true;
-
-
-    const messagesRef =
-        ref(
-            db,
-            "rooms/general/messages"
-        );
-
-
-    onChildAdded(
-        messagesRef,
-        snapshot => {
-
-            const message =
-                snapshot.val();
-
-
-            if(!message){
-
-                return;
-
-            }
-
-
-            renderMessage(
-                message
-            );
-
-        }
-    );
-
-}
-
 
 // ==========================================
 // SEND MESSAGE
